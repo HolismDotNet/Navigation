9a0cc54d1969e079c5c11c7c185a4cc865662a42
Saeed Nemati <sn.1361@gmail.com>
2022-02-12 19:27:37 +0330

    committed by script

--- Api/Admin/MenuItemController.cs
+++ Api/Admin/MenuItemController.cs
@@ -3,4 +3,6 @@ namespace Navigation;
 public class MenuItemController : TreeController<MenuItemView, MenuItem>
 {
     public override TreeBusiness<MenuItemView, MenuItem> Business => new MenuItemBusiness();
+
+    public override ReadBusiness<MenuItemView> ReadBusiness => new MenuItemBusiness();
 }

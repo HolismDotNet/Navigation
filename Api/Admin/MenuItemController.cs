59533fcca94b8f6d07d9fe02fe4857bf64fceab9
Saeed Nemati <sn.1361@gmail.com>
2022-02-14 11:43:27 +0330

    committed by script

--- Api/Admin/MenuItemController.cs
+++ Api/Admin/MenuItemController.cs
@@ -12,4 +12,11 @@ public class MenuItemController : TreeController<MenuItemView, MenuItem>
         var hierarchy = new HierarchyBusiness().Create(new MenuItemBusiness().EntityType, title);
         menuItem.HierarchyGuid = hierarchy.Guid;
     };
+
+    public override Action<MenuItem> PreUpdate => menuItem => 
+    {
+        var title = HttpContext.ExtractProperty("Title").ToString();
+        var hierarchyGuid = Business.Get(menuItem.Id).HierarchyGuid;
+        new HierarchyBusiness().UpdateTitle(hierarchyGuid, title);
+    };
 }

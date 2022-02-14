3e6a09e45fe415dbd00fdf633c6fb39151846231
Saeed Nemati <sn.1361@gmail.com>
2022-02-14 15:07:03 +0330

    committed by script

--- Api/Admin/MenuItemController.cs
+++ Api/Admin/MenuItemController.cs
@@ -9,14 +9,21 @@ public class MenuItemController : TreeController<MenuItemView, MenuItem>
     public override Action<MenuItem> PreCreation => menuItem =>
     {
         var title = HttpContext.ExtractProperty("Title").ToString();
-        var hierarchy = new HierarchyBusiness().Create(new MenuItemBusiness().EntityType, title);
+        var parentId = HttpContext.ExtractProperty("parentId");
+        var hierarchy = new HierarchyBusiness().Create(new MenuItemBusiness().EntityType, title, parentId == null ? null : parentId.ToString().ToLong());
         menuItem.HierarchyGuid = hierarchy.Guid;
     };
 
-    public override Action<MenuItem> PreUpdate => menuItem => 
+    public override Action<MenuItem> PreUpdate => menuItem =>
     {
         var title = HttpContext.ExtractProperty("Title").ToString();
         var hierarchyGuid = Business.Get(menuItem.Id).HierarchyGuid;
         new HierarchyBusiness().UpdateTitle(hierarchyGuid, title);
+    };
+
+    public override Action<long> PreDeletion => id =>
+    {
+        var hierarchyGuid = Business.Get(id).HierarchyGuid;
+        new HierarchyBusiness().Delete(hierarchyGuid);
     };
 }

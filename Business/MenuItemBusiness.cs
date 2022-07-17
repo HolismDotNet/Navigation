07e489d25020767bb05d033e9cdd53355f0b4300
Saeed Nemati <sn.1361@gmail.com>
2022-07-17 18:25:31 +0430

    committed by script

--- Business/MenuItemBusiness.cs
+++ Business/MenuItemBusiness.cs
@@ -6,8 +6,27 @@ public class MenuItemBusiness : TreeBusiness<MenuItemView, MenuItem>
 
     protected override Write<MenuItem> Write => Repository.MenuItem;
 
+    public object GetMenu()
+    {
+        var menu = Cache.Get("Menu");
+        if (menu != null) 
+        {
+            return menu;
+        }
+        menu = GetTree();
+        return menu;
+    }
+
     protected override long GetNodeId(MenuItemView node)
     {
         return node.HierarchyId;
+    }
+
+    public Dictionary<string, List<MenuItemView>> LoadCache()
+    {
+        var result = new Dictionary<string, List<MenuItemView>>();
+        var menu = GetTree();
+        result.Add("Menu", menu);
+        return result;
     }
 }
